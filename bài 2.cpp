ae647c87c9118f4bd8ec6f575343becc1521c1cc
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2022-08-04 00:04:19 +0700

    Create bài 2.cpp

---
+++ bài 2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	if(a > 0){
+		printf("%d la so duong",a);
+	}else{
+		if(a < 0){
+			printf("%d la so am",a);
+		}else{
+		if(a == 0){
+			printf("%d bang 0",a); 
+		}
+		 
+		
+	} 
+		
+		}
+	
+		 
+	}
