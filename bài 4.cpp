d7d3af7204a2eba2cb0a52330f5f67cacb0cad36
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2022-08-04 21:14:39 +0700

    Create bài 4.cpp

---
+++ bài 4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+
+	int b;
+	printf("Nhap b=");
+	scanf("%d",&b);
+	
+	int c;
+	printf("Nhap c=");
+	scanf("%d",&c);
+	if(a>b & a>c)
+		printf("%d la so lon nhat",a);
+	else
+		if(b>c)
+			printf("%d la so lon nhat",b);
+	else
+			printf("%d la so lon nhat",c); 
+		 
+		}
+		
+		
