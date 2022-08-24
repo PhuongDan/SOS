697e4e2cfe6e816fe8a1622d5387e66fb65ee758
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2022-08-24 15:59:03 +0700

    Update Exercise 2.cpp

--- Exercise 2.cpp
+++ Exercise 2.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
-int main(){
-	int n ,i, x, kt, ktgn;
-	printf("Nhap kich thuoc cua tap hop :");
-	scanf("%d",&n);
-	int arr[n]; 
-	printf("Nhap cac phan tu cua tap hop:\n");
-	for(int i=0;i<n;i++){
-		scanf("%d",&arr[i]);
-	for(i<arr[0];i>=0;i--){
-		printf("%d ", arr[i]);
-	}
+void reverseArray(int ary[]){
+	int i; 
+	for(i<ary[0];i>=0;i--){
+		printf("%d ", ary[i]); 
 }
 }
-
-
+	int main(void)
+{
+    int ary[3] = {1,2,3};
+printf("%d",ary[3]);
+}
+	
