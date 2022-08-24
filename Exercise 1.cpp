97efabd5a7bded4ebdd100bacb1e4a24cb94d779
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2022-08-24 15:15:23 +0700

    Create Exercise 1.cpp

---
+++ Exercise 1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n; 
+    float s=0; 
+    printf("Nhap n=");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){     
+	s+=(float)1/i;
+    }  
+    printf("S=%f",s);
+}
